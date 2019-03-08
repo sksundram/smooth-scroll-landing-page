8fd79d7b7147c72dfe13150682988f88476371ec
shailesh <shailesh.sundram@gmail.com>
2019-03-09 02:28:02 +0530

    Add smooth scroll using Smooth Scroll script

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
 // Option 2 - jQuery Smooth Scroll
-$('.navbar a').on('click', function(e) {
-  // Check for hash values in href like #home, #about, etc
-  if (this.hash !== '') {
-    // Prevent default behavior
-    e.preventDefault();
+// $('.navbar a').on('click', function(e) {
+//   // Check for hash values in href like #home, #about, etc
+//   if (this.hash !== '') {
+//     // Prevent default behavior
+//     e.preventDefault();
 
-    const hash = this.hash;
+//     const hash = this.hash;
 
-    $('html, body').animate(
-      {
-        scrollTop: $(hash).offset().top
-      },
-      500
-    );
-  }
+//     $('html, body').animate(
+//       {
+//         scrollTop: $(hash).offset().top
+//       },
+//       500
+//     );
+//   }
+// });
+
+// Option 3 - SmoothScroll Script
+const scroll = new SmoothScroll('.navbar a[href*="#"]', {
+  speed: 500
 });
